f5e802f90ef80df1d9b380392d56add412bb5ee2
Sam Genduso <sam.genduso@gmail.com>
2015-06-02 23:14:49 -0600

    removed prompt, replaced with form input for keyword search

--- js/index.js
+++ js/index.js
@@ -1,10 +1,38 @@
-var search = prompt("I want to see movies about...")
+var form = document.createElement('form');
+  form.setAttribute('action', '/index.js');
+  form.setAttribute('method', 'get');
+var label = document.createElement('label');
+  label.setAttribute('for','search');
+  label.innerHTML="I want to see movies about  ";
+var input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.setAttribute('id', 'search');
+var button = document.createElement('button');
+  button.setAttribute('type', 'submit');
+  button.innerHTML="SEARCH";
+form.appendChild(label);
+form.appendChild(input);
+form.appendChild(button);
+document.body.appendChild(form);
+var divContent = document.createElement('div');
+divContent.innerHTML="";
+document.body.appendChild(divContent);
+
+
+button.addEventListener('click', function () {
+  event.preventDefault();
+
+divContent.innerHTML="";
+
+
+var search = input.value;
 
 var h1 = document.createElement('h1');
 h1.className = 'upper';
 h1.innerHTML = 'OMDB Movies About' + " " + search;
-document.body.appendChild(h1);
+divContent.appendChild(h1);
 
+// var search = prompt("I want to see movies about...")
 
 var xhr = new XMLHttpRequest();
 xhr.open('get', 'http://www.omdbapi.com/?s=' + search);
@@ -22,8 +50,10 @@ for (var i = 0; i < responseData.Search.length; i++) {
 
   var p = document.createElement('p');
   p.appendChild(a);
-  document.body.appendChild(p);
+  divContent.appendChild(p);
 }
 
 });
 xhr.send();
+
+})
